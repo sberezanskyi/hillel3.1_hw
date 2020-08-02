ea3c32108727018cb9571301d98d8a11647d6032
Sergey <sberezanskyi@gmail.com>
2020-08-02 21:25:00 +0300

    add line 4

--- src/main/java/helloMaven.java
+++ src/main/java/helloMaven.java
@@ -1,5 +1,6 @@
 public class helloMaven {
     public static void main(String[] args) {
-        System.out.println("Hello Maven");
+        System.out.println("Hello Maven 1");
+        System.out.println("Hello Maven 2");
     }
 }
